5def5123839a0faca9cc377c4c5725cc98285904
Jun Hui <12046690+junhuiw@users.noreply.github.com>
2023-04-27 11:22:16 +0800

    updated endpoint to eb

--- src/Endpoints.tsx
+++ src/Endpoints.tsx
@@ -1,7 +1,10 @@
 const Endpoints = {
-    ACCOUNT_FOOD_BASE:"http://localhost:3001/api/v1",
-    AUTH_BASE:"http://localhost:3001/api/v1/auth",
-    DASHBOARD_BASE:"http://localhost:3001/api/v1/dashboard",
+    // ACCOUNT_FOOD_BASE:"http://localhost:3001/api/v1",
+    // AUTH_BASE:"http://localhost:3001/api/v1/auth",
+    // DASHBOARD_BASE:"http://localhost:3001/api/v1/dashboard",
+    ACCOUNT_FOOD_BASE:"http://ncaters-backend-accountandfood-env.eba-3zmmaxww.ap-southeast-2.elasticbeanstalk.com/api/v1",
+    AUTH_BASE:"http://ncaters-backend-accountandfood-env.eba-3zmmaxww.ap-southeast-2.elasticbeanstalk.com/api/v1/auth",
+    DASHBOARD_BASE:"http://ncaters-backend-accountandfood-env.eba-3zmmaxww.ap-southeast-2.elasticbeanstalk.com/api/v1/dashboard",
     NOTIF_REWARD_BASE:"http://localhost:3002/api/v1"
 }
 
