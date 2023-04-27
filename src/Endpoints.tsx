9c4ab4b2dc9d9df5159fbbf1904d836afedb9567
Jun Hui <12046690+junhuiw@users.noreply.github.com>
2023-04-27 18:17:35 +0800

    Updated notificationAndReward endpoint

--- src/Endpoints.tsx
+++ src/Endpoints.tsx
@@ -2,10 +2,11 @@ const Endpoints = {
     // ACCOUNT_FOOD_BASE:"http://localhost:3001/api/v1",
     // AUTH_BASE:"http://localhost:3001/api/v1/auth",
     // DASHBOARD_BASE:"http://localhost:3001/api/v1/dashboard",
+    // NOTIF_REWARD_BASE:"http://localhost:3002/api/v1"
     ACCOUNT_FOOD_BASE:"http://ncaters-backend-accountandfood-env.eba-3zmmaxww.ap-southeast-2.elasticbeanstalk.com/api/v1",
     AUTH_BASE:"http://ncaters-backend-accountandfood-env.eba-3zmmaxww.ap-southeast-2.elasticbeanstalk.com/api/v1/auth",
     DASHBOARD_BASE:"http://ncaters-backend-accountandfood-env.eba-3zmmaxww.ap-southeast-2.elasticbeanstalk.com/api/v1/dashboard",
-    NOTIF_REWARD_BASE:"http://localhost:3002/api/v1"
+    NOTIF_REWARD_BASE:"http://http://ncaters-backend-rewardandnotific-env.eba-anzbrs3u.ap-southeast-2.elasticbeanstalk.com/api/v1"
 }
 
 export default Endpoints;
